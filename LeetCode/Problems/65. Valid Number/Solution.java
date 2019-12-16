b57e7f051b7a78fd2d7797d7e3666c33752e9477
Fred <8eatles@naver.com>
2019-12-16 12:45:35 +0900

    Update Solution.java

--- LeetCode/Problems/65. Valid Number/Solution.java
+++ LeetCode/Problems/65. Valid Number/Solution.java
@@ -1,18 +1,18 @@
 class Solution {
-    public boolean isNumber(String s) {
-        String noSpace = s.trim();
-        if (noSpace.length() == 0) return false;
-        char first = noSpace.charAt(0);
-        if (first < '0' || first > '9') {
-        	if (noSpace.length() == 1 || (first != '+' && first != '-' && first != '.')) {
-        		return false;
-        	}
-        }
-        boolean numericBeforeE = false;
-        boolean numericAfterE = false;
-    	boolean exponent = false;
-    	boolean exponentedNow = false;
-    	boolean point = false;
+	public boolean isNumber(String s) {
+		String noSpace = s.trim();
+		if (noSpace.length() == 0) return false;
+		char first = noSpace.charAt(0);
+		if (first < '0' || first > '9') {
+			if (noSpace.length() == 1 || (first != '+' && first != '-' && first != '.')) {
+				return false;
+			}
+		}
+		boolean numericBeforeE = false;
+		boolean numericAfterE = false;
+		boolean exponent = false;
+		boolean exponentedNow = false;
+		boolean point = false;
 		for(int i = 0; i < noSpace.length(); i += 1) {
 			char c = noSpace.charAt(i);
 			if (c >= '0' && c <= '9') {
@@ -22,8 +22,8 @@ class Solution {
 				else
 					numericBeforeE = true;
 			}
-            else if (c == 'e' && i != 0 && exponent == false && i != noSpace.length() - 1)
-            	exponent = exponentedNow = true;
+			else if (c == 'e' && i != 0 && exponent == false && i != noSpace.length() - 1)
+					exponent = exponentedNow = true;
 			else if (c == '.' && exponent == false && point == false)
 				point = true;
 			else if (c == '+' && (i == 0 || exponentedNow))
@@ -33,6 +33,7 @@ class Solution {
 			else
 				return false;
 		}
+			
 		return (numericAfterE || !exponent) && numericBeforeE;
-    }
+	}
 }
